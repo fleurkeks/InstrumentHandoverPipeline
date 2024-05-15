fixed rotation

Training/MPHandler.py
@@ -51,7 +51,7 @@ def angle_between_vectors(v1, v2):
 
     cos_angle = dot_product / (magnitude1 * magnitude2)
 
-    angle_rad = np.arccos(cos_angle)
+    angle_rad = abs(np.arccos(cos_angle))
 
     return angle_rad
 
@@ -64,23 +64,23 @@ def hand_quaternion(lm0, lm5, lm17):
     #normalen till handplan, lokal koordinater
     n=np.cross(v1,v2)
 
-    #find rotation relative to the z axis pointing, pointing away from cam
-    ogRot=np.array((0,0,1))
+    #find rotation relative to the z axis pointing, pointing away from cam, find rotation in cameras coordinate system
+    zAxis=np.array((0,0,1))
 
     #check 
-    if(are_antiparallel(ogRot,n)):
-        axis_of_rotation=(0,1,0)
+    if(are_antiparallel(zAxis,n)):
+        axis_of_rotation=(1,0,0)
+        angle_of_rotation=np.pi
     
     #calculate the normal to the original dirVec of the hand and the current dirVec of the hand, that gives the axis of rotation
     else:
-        axis_of_rotation=np.cross(ogRot,n)
+        axis_of_rotation=np.cross(zAxis,n)
          #check if the axis of rotation is 0, that would mean no rotation has happened --> return identity quaternion
         if(is_null_vector(axis_of_rotation)):
             return Quaternion(1, 0, 0, 0)
+        #find the angle of rotation by finding the angle between original orientation and current orientation
+        angle_of_rotation=angle_between_vectors(zAxis,n)
     
-
-    #find the angle of rotation by finding the angle between original orientation and current orientation
-    angle_of_rotation=angle_between_vectors(ogRot,n)
 
     #using this we can easly calculate the rotation in quaternions
     q = Quaternion(axis = axis_of_rotation, angle = angle_of_rotation)